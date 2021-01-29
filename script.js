3e0f5e1b0d4cc74078730742de2c44a6e3cd7cdc
themanwithsuds <user5792023@teams.noreply.repl.it>
2021-01-29 13:09:56 +0000

    added some java

---
+++ script.js
@@ -0,0 +1,58 @@
+const cardContainer = document.querySelector("cardContainer");
+let cards = [];
+fetch("data.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    cards = data;
+    appendData(cards);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+  function appendData(data) {
+  var cardContainer = document.getElementById("cardContainer");
+  cardContainer.innerHTML = "";
+
+  for (var i = 0; i < data.length; i++) {
+    var card = document.createElement("div");
+    card.className = "card";
+    cardContainer.appendChild(card);
+
+    var img = document.createElement("img");
+    img.src = data[i].src;
+    card.appendChild(img);
+
+    var tagContainer = document.createElement("div");
+    tagContainer.className = "tagContainer";
+    card.appendChild(tagContainer);
+
+    const tagButtons = data[i].tags.map((tag) => {
+      const tagButton = document.createElement("button");
+      tagButton.innerHTML = tag;
+      return tagButton;
+    });
+    for (const tagButton of tagButtons) {
+      tagButton.className = "tagButton";
+      tagContainer.appendChild(tagButton);
+    }
+  }
+}
+function filterTags() {
+  var searchTerm = document.getElementById("searchInput").value;
+  document.getElementById("searchResult").innerHTML =
+    "You searched for: " + searchTerm;
+
+  const searchTermLower = searchTerm.toLowerCase();
+
+  const filteredCards = cards.filter((card) => {
+    return (
+      card.tags.find((tag) => {
+        const tagLower = tag.toLowerCase();
+        return tagLower.includes(searchTermLower);
+      }) !== undefined
+    );
+  });
+  appendData(filteredCards);
+}
